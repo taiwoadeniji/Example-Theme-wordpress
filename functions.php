8a5b0a46bf8d263bb19702fb77a8d69e382d8ae4
Taiwo Adeniji <taiwoadeniji19@gmail.com>
2020-05-04 17:13:35 -0600

    functions.php error free

--- functions.php
+++ functions.php
@@ -1,41 +1,41 @@
 <?php
 /**
- * PHP fuctions / logic we can run before our template outpot.
- * 
+ * PHP functions / logic we can run before our template(s) output.
  */
 
- add_action('wp_enqueue_scripts', function(){
-     // Themes script.
-wp_enqueue_scripts(
+add_action( 'wp_enqueue_scripts', function() {
+  // Theme script.
+  wp_enqueue_script(
     // Name of script.
     'example-theme-scripts',
-    //Public path to script.
-get_theme_file_uri(
-    '/assets/js/scripts.js'
-),
-    // Dependency scripts (ny name, in array.)
-array(),
+    // Public path to script.
+    get_theme_file_uri(
+      '/assets/js/scripts.js'
+    ),
+    // Dependency scripts (by name, in array.)
+    array(),
     // Should a version number be appended?
-strftime(get_theme_file_uri(
-    '/assets/css/main.css'
-)),
-    //Output in head.
+    strftime( get_theme_file_uri(
+      '/assets/js/scripts.js'
+    ) ),
+    // Output in head.
     TRUE
-);
-    // Theme styles.
-wp_enqueue_style(
+  );
+
+  // Theme styles.
+  wp_enqueue_style(
     'example-theme-styles',
     // Public path to script.
-get_theme_file_uri(
+    get_theme_file_uri(
       '/assets/css/main.css'
     )
   );
- });
+} );
 
-  // Add navigation menus to WordPress.
+// Add navigation menus to WordPress.
 register_nav_menus(
-    array(
-        'main_menu' => 'Main Menu'
-        )
-      );
-      
+  array(
+    'main_menu' => 'Main Menu'
+  )
+);
+
